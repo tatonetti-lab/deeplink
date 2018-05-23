386843a5e5c1b4d3527f16b154bf7cf93a7ac4c4
Nick <nick.tatonetti@columbia.edu>
2018-05-23 10:41:04 -0400

    modified job script

--- src/generate_slice_jobs.py
+++ src/generate_slice_jobs.py
@@ -7,4 +7,4 @@ njobs = 30
 neventsperjob = nevents/njobs
 
 for i in range(0,nevents+1,neventsperjob):
-    print "python src/correlated_but_not_slice.py %d:%d" % (i, i+neventsperjob)
+    print "bsub python src/correlated_but_not_slice.py %d:%d" % (i, i+neventsperjob)
